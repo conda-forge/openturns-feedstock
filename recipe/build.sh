0f787de0963dd1196f54985f4f6b68ed92ff9f0f
Julien Schueller <schueller@phimeca.com>
2016-06-30 18:56:24 +0200

    Update to 1.8rc1

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,12 +11,13 @@ fi
 mkdir -p build && cd build
 
 cmake \
-  -G Ninja \
   -DCMAKE_FIND_ROOT_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DLAPACKE_FOUND=TRUE -DOPENTURNS_LIBRARIES="$PREFIX/lib/libopenblas${SO_EXT}" \
   -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
+  -DUSE_COTIRE=ON -DCOTIRE_TESTS=OFF -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j${CPU_COUNT}" \
   ..
 
-ninja install
-DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib ctest -R pyinstallcheck --output-on-failure ${MAKEFLAGS} 
+make python_unity -j${CPU_COUNT}
+make install/fast
+DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT}
