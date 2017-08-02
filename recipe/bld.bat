4d9016748566f003b3bb7db9b8eedf20f79c403f
Julien Schueller <schueller@phimeca.com>
2017-08-02 18:54:13 +0200

    Try use -mstackrealign on 32 bits

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,9 @@
 :: remove sh.exe from PATH
 set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 
+:: swig modules need proper stack alignment on 32 bit
+if "%ARCH%"=="32" (set SWIG_CXX_FLAGS="-mstackrealign") else (set SWIG_CXX_FLAGS="")
+
 mkdir build && cd build
 
 :: Configure.
@@ -11,7 +14,7 @@ cmake -G "MinGW Makefiles" ^
   -DLAPACK_LIBRARIES=%LIBRARY_PREFIX%/mingw-w64/lib/libopenblas.dll.a ^
   -DPYTHON_LIBRARY=%PREFIX%\libs\libpython%CONDA_PY%.dll.a ^
   -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4" ^
-  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4" ^
+  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 %SWIG_CXX_FLAGS%" ^
   -DSITE_PACKAGE=%SP_DIR% ^
   ..
 if errorlevel 1 exit 1
