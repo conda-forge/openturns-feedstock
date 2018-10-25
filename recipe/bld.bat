51645e8688195d4c0757ae2182e69ad07570f158
Julien Schueller <schueller@phimeca.com>
2018-10-25 10:15:46 +0200

    No muparser

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,7 @@ cmake -G "MinGW Makefiles" ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DLAPACK_LIBRARIES=%LIBRARY_PREFIX:\=/%/mingw-w64/lib/libopenblas.dll.a ^
-  -DPYTHON_LIBRARY=%PREFIX%/libs/libpython%CONDA_PY%.dll.a ^
+  -DPYTHON_LIBRARY=%PREFIX:\=/%/libs/libpython%CONDA_PY%.dll.a ^
   -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64" ^
   -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64 -D_hypot=hypot" ^
   -DPYTHON_SITE_PACKAGES="%SP_DIR:\=/%" ^
