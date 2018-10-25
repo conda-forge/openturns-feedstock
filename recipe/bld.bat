28f94b32748de2a4a6b4a64d5b369a655edf34dc
Julien Schueller <schueller@phimeca.com>
2018-10-25 11:25:14 +0200

    Disable EXPRTK

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,17 +10,19 @@ cmake -G "MinGW Makefiles" ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DLAPACK_LIBRARIES=%LIBRARY_PREFIX:\=/%/mingw-w64/lib/libopenblas.dll.a ^
   -DPYTHON_LIBRARY=%PREFIX:\=/%/libs/libpython%CONDA_PY%.dll.a ^
-  -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64" ^
-  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64 -D_hypot=hypot" ^
+  -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4" ^
+  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4" ^
   -DPYTHON_SITE_PACKAGES="%SP_DIR:\=/%" ^
-  -DBUILD_SHARED_LIBS=OFF ^
   -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j16" ^
+  -DUSE_EXPRTK=OFF ^
   ..
+:: note: disable ExprTk because linking takes forever
 if errorlevel 1 exit 1
 
 :: Build.
 mingw32-make install -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
-ctest -R pyinstallcheck --output-on-failure -j %CPU_COUNT%
+:: Run features test.
+python ..\python\test\t_features.py
 if errorlevel 1 exit 1
