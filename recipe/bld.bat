eb77de3783a03b3dcf9f8abe18e0a1287874551f
Julien Schueller <schueller@phimeca.com>
2018-10-25 09:51:01 +0200

    LAPACK_LIBRARIES

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ mkdir build && cd build
 cmake -G "MinGW Makefiles" ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
-  -DLAPACK_LIBRARIES=%LIBRARY_PREFIX%/mingw-w64/lib/libopenblas.dll.a ^
+  -DLAPACK_LIBRARIES=%LIBRARY_PREFIX:\=/%/mingw-w64/lib/libopenblas.dll.a ^
   -DPYTHON_LIBRARY=%PREFIX%/libs/libpython%CONDA_PY%.dll.a ^
   -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64" ^
   -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64 -D_hypot=hypot" ^
