a15ceb9c446fd9be2e1ffd00f74d78510e9a65ac
Julien Schueller <schueller@phimeca.com>
2018-11-08 14:26:11 +0100

    Update to 1.12

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,10 @@
 
 curl -fsSLO https://github.com/openturns/build/releases/download/v%PKG_VERSION%/openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe
+if errorlevel 1 exit 1
+
 openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe /userlevel=1 /S /FORCE /D=%PREFIX%
+if errorlevel 1 exit 1
+
 exit /b 0
 
 : remove sh.exe from PATH
