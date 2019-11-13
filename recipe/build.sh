7dd1dbeaa5bbf56ad7d9d41105958805fac87da3
Julien Schueller <schueller@phimeca.com>
2019-11-13 14:50:43 +0100

    Update to 1.14

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,9 +11,9 @@ cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_MACOSX_RPATH=ON \
-  -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j${CPU_COUNT}" \
+  -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j8" \
   -DSWIG_COMPILE_FLAGS="-O1" \
   ..
 make install -j${CPU_COUNT}
 rm -r ${PREFIX}/share/gdb
-ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E "docstring_model_copula|Function_python"
+ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E docstring_model_copula
