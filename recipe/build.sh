62e5284bb3b1f71f0a7abc973ee5aa105c9b79a0
Julien Schueller <schueller@phimeca.com>
2020-04-10 14:34:14 +0200

    Update for 1.15rc1

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,9 +11,9 @@ cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_MACOSX_RPATH=ON \
-  -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j8" \
+  -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   -DSWIG_COMPILE_FLAGS="-O1" \
   ..
 make install -j${CPU_COUNT}
 rm -r ${PREFIX}/share/gdb
-ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E docstring_model_copula
+ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT}
