799381e80427e13e20686bd87fc027646f89a60f
Julien Schueller <schueller@phimeca.com>
2020-04-11 15:20:34 +0200

    Run tests on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,3 +5,8 @@ if errorlevel 1 exit 1
 openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe /userlevel=1 /S /FORCE /D=%PREFIX%
 if errorlevel 1 exit 1
 
+cmake -LAH -G"NMake Makefiles" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DBLAS_LIBRARIES=1 -DLAPACK_LIBRARIES=1 .
+if errorlevel 1 exit 1
+
+ctest -R pyinstallcheck --output-on-failure -E docstring
+if errorlevel 1 exit 1
