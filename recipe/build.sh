b9322a1b9c5dd88592b22c9594155bb8d86307f0
Julien Schueller <schueller@phimeca.com>
2021-10-19 11:40:51 +0200

    Update to 1.18rc1

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,9 +13,9 @@ cmake -LAH \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_MACOSX_RPATH=ON \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
   -DSWIG_COMPILE_FLAGS="-O1" \
-  -DR_EXECUTABLE="dummyR" \
+  -DUSE_R=OFF \
   -DUSE_BONMIN=OFF \
   ..
 make install -j${CPU_COUNT}
 rm -r ${PREFIX}/share/gdb
-ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E GeneralizedParetoFactory_std
+ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E "GeneralizedParetoFactory_std|KrigingAlgorithm_std"
