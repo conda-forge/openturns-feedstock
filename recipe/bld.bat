3e4024f8048970e4e412c1900ae2b6ca2313f841
Julien Schueller <schueller@phimeca.com>
2023-06-02 10:49:18 +0200

    try persalys

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,46 @@
-del /s /q "C:\Program Files\R"
 
-curl -fsSLO https://github.com/openturns/build/releases/download/v%PKG_VERSION%/openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe
+:: remove -GL from CXXFLAGS
+set "CXXFLAGS=-MD"
+
+:: from Azure
+set "Boost_ROOT="
+
+git clone -b 1.21 --depth 1 https://github.com/openturns/openturns.git
+cd openturns
+
+mkdir build && cd build
+cmake -LAH -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
+    -DCMAKE_INSTALL_BINDIR="Library/bin" ^
+    -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
+    -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
+    -DPython_FIND_STRATEGY=LOCATION ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    ..
 if errorlevel 1 exit 1
 
-openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe /userlevel=1 /S /FORCE /D=%PREFIX%
+cmake --build . --target install --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
 
-cmake -LAH -G"NMake Makefiles" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DBLAS_LIBRARIES=1 -DLAPACK_LIBRARIES=1 -DPython_FIND_STRATEGY=LOCATION -DPython_ROOT_DIR="%PREFIX%" .
+git clone -b msvc --depth 1 https://github.com/persalys/persalys.git
+cd persalys
+
+mkdir build && cd build
+cmake -LAH -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
+    -DCMAKE_INSTALL_BINDIR="Library/bin" ^
+    -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
+    -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
+    -DPython_FIND_STRATEGY=LOCATION ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    ..
 if errorlevel 1 exit 1
 
-ctest -R pyinstallcheck --output-on-failure --timeout 1000 -E "docstring|example|PlotDesign"
+cmake --build . --target install --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
+git clone -b msvc --depth 1 https://github.com/persalys/persalys.git
+cd persalys
+
 mkdir build && cd build
 
-cmake ${CMAKE_ARGS} -LAH \
+cmake ${CMAKE_ARGS} -LAH -G "Ninja" \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
   -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
   -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
-  -DSWIG_COMPILE_FLAGS="-O1" \
-  -D_NLOPT_STOGO_RUNS=1 -D_NLOPT_AGS_RUNS=1 -D_HAVE_FR_LOC_RUNS=0 \
   ..
-make install -j${CPU_COUNT}
-rm -r ${PREFIX}/share/gdb
+cmake --build . --target instal
 if test "$CONDA_BUILD_CROSS_COMPILATION" != "1"
 then
-  ctest -R pyinstallcheck --output-on-failure --schedule-random -j${CPU_COUNT} -E "GeneralizedParetoFactory_std|KrigingAlgorithm_std|ChaosSobol"
+  ctest -R pyinstallcheck --output-on-failure --schedule-random -j${CPU_COUNT}
 fi
