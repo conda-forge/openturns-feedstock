25289244ec4a8142fd52cf65e98d5b0e053201f7
Julien Schueller <schueller@phimeca.com>
2023-06-05 09:33:55 +0200

    QWT_DLL

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,29 @@ set "Boost_ROOT="
 git clone -b misc --depth 1 https://github.com/jschueller/openturns.git
 cd openturns
 
+
+mkdir build && cd build
+cmake -LAH -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
+    -DCMAKE_INSTALL_BINDIR="Library/bin" ^
+    -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
+    -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
+    -DPython_FIND_STRATEGY=LOCATION ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    ..
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+
+xcopy /y /s /f %LIBRARY_PREFIX%\bin\OT.dll %SP_DIR%\openturns
+
+git clone --depth 1 https://github.com/openturns/otmorris.git
+cd otmorris
+
 mkdir build && cd build
 cmake -LAH -G "Ninja" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
@@ -25,6 +48,9 @@ if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
+
+xcopy /y /s /f %LIBRARY_PREFIX%\bin\otmorris.dll %SP_DIR%\otmorris
+
 
 git clone -b msvc --depth 1 https://github.com/persalys/persalys.git
 cd persalys
@@ -38,7 +64,6 @@ cmake -LAH -G "Ninja" ^
     -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
     -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
-    -DCMAKE_CXX_FLAGS="/DQWT_DLL" ^
     -DPython_FIND_STRATEGY=LOCATION ^
     -DPython_ROOT_DIR="%PREFIX%" ^
     ..
@@ -46,3 +71,5 @@ if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
+
+xcopy /y /s /f %LIBRARY_PREFIX%\bin\persalys*.dll %SP_DIR%\persalys
