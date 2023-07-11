35e64405a44d908d10f1dcfb79b34a2eca88d3c2
Julien Schueller <schueller@phimeca.com>
2023-07-11 14:46:19 +0200

    Test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,3 @@
-del /s /q "C:\Program Files\R"
-
-curl -fsSLO https://github.com/openturns/build/releases/download/v%PKG_VERSION%/openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe
-if errorlevel 1 exit 1
-
-openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe /userlevel=1 /S /FORCE /D=%PREFIX%
-if errorlevel 1 exit 1
-
-cmake -LAH -G "Ninja" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DBLAS_LIBRARIES=1 -DLAPACK_LIBRARIES=1 -DPython_FIND_STRATEGY=LOCATION -DPython_ROOT_DIR="%PREFIX%" .
-if errorlevel 1 exit 1
-
-ctest -R pyinstallcheck --output-on-failure --timeout 1000 -E "docstring|example|PlotDesign"
+python -c "from PyQt5.QtWidgets import QApplication, QWidget; app = QApplication([]); print('ok')"
+python -c "from PySide6.QtWidgets import QApplication, QWidget; app = QApplication([]); print('ok')"
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-mkdir build && cd build
+python -c "from PyQt5.QtWidgets import QApplication, QWidget; app = QApplication([]); print('ok')"
+python -c "from PySide6.QtWidgets import QApplication, QWidget; app = QApplication([]); print('ok')"
 
-cmake ${CMAKE_ARGS} -LAH -G "Ninja" \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_FIND_FRAMEWORK=NEVER \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
-  -DPython_FIND_STRATEGY=LOCATION \
-  -DPython_ROOT_DIR=${PREFIX} \
-  -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
-  -DSWIG_COMPILE_FLAGS="-O1" \
-  -D_NLOPT_STOGO_RUNS=1 -D_NLOPT_AGS_RUNS=1 -D_HAVE_FR_LOC_RUNS=0 \
-  ..
-cmake --build . --target install
-rm -r ${PREFIX}/share/gdb
-
-if test "$CONDA_BUILD_CROSS_COMPILATION" != "1"
-then
-  ctest -R pyinstallcheck --output-on-failure --schedule-random -j${CPU_COUNT} -E "GeneralizedParetoFactory_std|KrigingAlgorithm_std|ChaosSobol"
-fi
