e5e7da1907988fded6b5728759441d5ebc1e45c6
Julien Schueller <schueller@phimeca.com>
2023-08-29 11:26:48 +0200

    vs

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,25 @@
-del /s /q "C:\Program Files\R"
 
-curl -fsSLO https://github.com/openturns/build/releases/download/v%PKG_VERSION%/openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe
+:: remove -GL from CXXFLAGS
+set "CXXFLAGS=-MD"
+
+:: from Azure
+set "Boost_ROOT="
+
+mkdir build && cd build
+cmake -LAH -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
+    -DCMAKE_INSTALL_BINDIR="Library/bin" ^
+    -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
+    -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DPython_FIND_STRATEGY=LOCATION ^
+    -DPython_ROOT_DIR="%PREFIX%" ^
+    ..
 if errorlevel 1 exit 1
 
-openturns-%PKG_VERSION%-py%PY_VER%-x86_64.exe /userlevel=1 /S /FORCE /D=%PREFIX%
+cmake --build . --target install --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
 
-cmake -LAH -G "Ninja" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DBLAS_LIBRARIES=1 -DLAPACK_LIBRARIES=1 -DPython_FIND_STRATEGY=LOCATION -DPython_ROOT_DIR="%PREFIX%" .
-if errorlevel 1 exit 1
-
-ctest -R pyinstallcheck --output-on-failure --timeout 1000 -E "docstring|example|PlotDesign"
+ctest --config Release -R pyinstallcheck --output-on-failure --timeout 1000
 if errorlevel 1 exit 1
