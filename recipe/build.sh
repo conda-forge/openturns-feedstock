ba0b68aff7bd42b667359a68f70bad2e6700860b
Julien Schueller <schueller@phimeca.com>
2024-06-06 16:59:04 +0200

    asl

--- recipe/build.sh
+++ recipe/build.sh
@@ -50,7 +50,22 @@ cd -
 
 git clone -b releases/1.8.9 https://github.com/coin-or/Bonmin.git
 cd Bonmin
-CXXFLAGS="${CXXFLAGS} -g" ./configure --prefix=${PREFIX}
+CXXFLAGS="${CXXFLAGS} -g" LIBS="-lCoinUtils -lOsi -lCgl" ./configure --prefix=${PREFIX} \
+  --with-coinutils-lib="$(pkg-config --libs coinutils)" \
+  --with-coinutils-incdir="${PREFIX}/include/coin/" \
+  --with-osi-lib="$(pkg-config --libs osi)" \
+  --with-osi-incdir="${PREFIX}/include/coin/" \
+  --with-clp-lib="$(pkg-config --libs clp)" \
+  --with-clp-incdir="${PREFIX}/include/coin/" \
+  --with-cgl-lib="$(pkg-config --libs cgl)" \
+  --with-cgl-incdir="${PREFIX}/include/coin/" \
+  --with-cbc-lib="$(pkg-config --libs cbc)" \
+  --with-cbc-incdir="${PREFIX}/include/coin/" \
+  --with-ipopt-lib="$(pkg-config --libs ipopt)" \
+  --with-ipopt-incdir="${PREFIX}/include/coin/" \
+  --with-asl-incdir="${PREFIX}/include/asl" \
+  --with-asl-lib="$(pkg-config --libs ipoptamplinterface) -lasl"
+
 make -j${CPU_COUNT}
 make install
 cd -
