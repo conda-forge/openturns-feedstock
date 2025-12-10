efcc0e048f9a2d5a3a735f2aafd3350f5cf6db6c
Julien Schueller <schueller@phimeca.com>
2025-12-10 16:24:52 +0100

    Disable primesieve

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,11 @@
-
-:: remove -GL from CXXFLAGS
-set "CXXFLAGS=-MD"
-
-:: from Azure
-set "Boost_ROOT="
-
-cmake -LAH -G "Ninja" ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+cmake %CMAKE_ARGS% -LAH -G "Ninja" ^
     -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
     -DBLA_VENDOR=Generic ^
     -DPython_FIND_STRATEGY=LOCATION ^
     -DPython_ROOT_DIR="%PREFIX%" ^
     -DOPENTURNS_PYTHON_MODULE_PATH=../Lib/site-packages ^
     -DSWIG_COMPILE_FLAGS="/DPy_LIMITED_API=0x030A0000" -DUSE_PYTHON_SABI=ON ^
+    -DUSE_PRIMESIEVE=OFF ^
     -B build .
 if errorlevel 1 exit 1
 
